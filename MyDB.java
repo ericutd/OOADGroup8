93315e46ecbc317d350eb8f2d86d91a0caeb8e58
csKid19 <zoo150030@utdallas.edu>
2018-09-30 02:23:34 -0500

    Reservation code
    Make sure to put the mysql-connector-java-5.1.46-bin.jar into the WebContent/WEB-INF/lib directory (this allows us to connect to a db).
    Also, the MyDb class is currently configured to connect to the database on my laptop, so you'll need to change the USER, PASS, and CONN_URL fields to match those that align with the db on your system.

---
+++ MyDB.java
@@ -0,0 +1,17 @@
+/* Author: Zack Oldham
+ * CS 6359.002
+ * 09/28/2018
+ * This interface defines the connection parameters for the connection to the parkingManagementSystem database. 
+ */
+
+
+
+public interface MyDB 
+{
+
+	String USER="root";
+	String PASS="data2018!";
+	String CONN_URL="jdbc:mysql://127.0.0.1:3306/parkingManagementSystem";
+	
+	
+}
