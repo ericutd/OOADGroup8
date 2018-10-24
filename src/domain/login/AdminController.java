b7a260debe4aa943827f01574a11b86cc73d0298
yxy175330 <33034607+yxy175330@users.noreply.github.com>
2018-10-24 12:44:46 -0500

    Update AdminController.java

--- src/domain/login/AdminController.java
+++ src/domain/login/AdminController.java
@@ -6,6 +6,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
 
 /**
  * Servlet implementation class AdminController
@@ -14,8 +16,23 @@ import javax.servlet.http.HttpServletResponse;
 public class AdminController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     
-    public AdminController() {}
+	public AdminController() {}
 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ParkingSpotsDao  psDao = new CategoryDAO();
+
+		try {
+
+		    List<int> listId = psDao.list();
+		    request.setAttribute("listId", listId);
+
+		    RequestDispatcher dispatcher = request.getRequestDispatcher("Admin.jsp");
+		    dispatcher.forward(request, response);
+
+		} catch (SQLException e) {
+		    e.printStackTrace();
+		    throw new ServletException(e);
+        }
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		int number = Integer.parseInt(request.getParameter("id"));
 		String colorPass = request.getParameter("permit");
