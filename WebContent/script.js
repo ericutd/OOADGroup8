1bebe6023af49f4f14190fbcdbf65ef74d3d936f
yxy175330 <33034607+yxy175330@users.noreply.github.com>
2018-10-24 08:59:15 -0500

    Update script.js

--- WebContent/script.js
+++ WebContent/script.js
@@ -1,18 +1,24 @@
 function regValidate() {
 
-	var username = document.forms["regform"]["username"].value;
 	var password = document.forms["regform"]["password"].value;
 	var rpassword = document.forms["regform"]["retry-password"].value;
+	var email = document.forms["regform"]["email"].value;
+	var dropdown = document.forms["regform"]["dropdown"].value;
 	
-	if (username == "") {
-        alert("username must be filled out");
-        document.forms["regform"]["username"].focus();
-        return false;
-    }else if (password== "") {
+	if (password== "") {
         alert("password must be filled out");
         document.forms["regform"]["password"].focus();
         return false;
-    }else if (rpassword == "") {
+    }else if (email== "") {
+        alert("Email must be filled out");
+        document.forms["regform"]["email"].focus();
+        return false;
+    }else if (dropdown== "") {
+        alert("Account Type must be filled out");
+        document.forms["regform"]["dropdown"].focus();
+        return false;
+    }    
+    else if (rpassword == "") {
         alert("retry-password must be filled out");
         document.forms["regform"]["retry-password"].focus();
         return false;
@@ -112,7 +118,18 @@ function vehicleForm(){
 	var buttonelement = document.createElement('input');
 	buttonelement.setAttribute("type", "button");
 	buttonelement.setAttribute("name", "add another vehicle");
-	buttonelement.setAttribute("onclick", "vehicleForm()");
+	buttonelement.setAttribute("onclick", "vehicleForm");
 	form.appendChild(buttonelement);
+
+}
+function hide(){
+	var x = document.getElementById("spots");
+	x.style.visibility = 'hidden';
+}
+function addSpots() {
+	var y = document.getElementById("spots");
+	y.style.visibility = 'visible';
+}
+function Add(){
 
 }
