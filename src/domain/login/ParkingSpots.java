36fe98ddd39872ffa745e00042db3012d4f35fe7
yxy175330 <33034607+yxy175330@users.noreply.github.com>
2018-10-24 09:02:04 -0500

    Create ParkingSpots.java

---
+++ src/domain/login/ParkingSpots.java
@@ -0,0 +1,40 @@
+package domain.login;
+
+public class ParkingSpots {
+	private int parkingSpotId;
+	private int parkingLotId;
+	private String LicenseNo;
+	private boolean occupied;
+	private String colorClass;
+
+	public int getParkingSpotId(){
+		return parkingSpotId;
+	}
+	public void setParkingSpotId(int parkingSpotId){
+		this.parkingSpotId = parkingSpotId;
+	}
+	public int getParkingLotId(){
+		return parkingLotId;
+	}
+	public void setParkingLotId(int parkingLotId){
+		this.parkingLotId = parkingLotId;
+	}
+	public String getLicenseNo(){
+		return LicenseNo;
+	}
+	public void setLicenseNo(String LicenseNo){
+		this.LicenseNo = LicenseNo;
+	}
+	public boolean getOccupied(){
+		return occupied;
+	}
+	public void setOccupied(boolean occupied){
+		this.occupied = occupied;
+	}
+	public String getColotPass(){
+		return colorClass;
+	}
+	public void setColorPass(String colorClass){
+		this.colorClass = colorClass;
+	}
+}
