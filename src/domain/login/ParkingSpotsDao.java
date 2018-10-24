9979989eb39674827032444476b26661fced4d8b
yxy175330 <33034607+yxy175330@users.noreply.github.com>
2018-10-24 12:36:18 -0500

    Update ParkingSpotsDao.java

--- src/domain/login/ParkingSpotsDao.java
+++ src/domain/login/ParkingSpotsDao.java
@@ -4,6 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import db.DbManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ParkingSpotsDao {
 	static Connection conn;
@@ -17,11 +21,11 @@ public class ParkingSpotsDao {
 		while(no != 0)
 		{
 			try{
-			conn = db.getConnection();
-			ps = conn.prepareStatement("insert into parkingSpot values(?,?)");
-			ps.setString(2, p.getParkingLotId());
-			ps.setString(5, p.getColorPass());
-			status = ps.executeUpdate();
+				conn = db.getConnection();
+				ps = conn.prepareStatement("insert into parkingSpot values(?,?)");
+				ps.setString(2, p.getParkingLotId());
+				ps.setString(5, p.getColorPass());
+				status = ps.executeUpdate();
 			}catch(Exception e){
 				System.out.println(e);
 			}
@@ -30,9 +34,24 @@ public class ParkingSpotsDao {
 		
 		return status;
 	}
-	public getLotId(){
-		conn = db.getConnection();
-		ps =conn.prepareStatement("select parkingLotId from parkingLot");
-		ResultSet rs = ps.executeQuery();
+	public List<int> list() throws SQLException {
+		List<int> lotId = new ArrayList<>();
+
+		try{
+			String sql = "SELECT parkingLotId FROM parkingLot";
+			Statement statement = connection.createStatement();
+			ResultSet result = statement.executeQuery(sql);
+
+			while (result.next()) {
+				int id = result.getInt("parkingLotId");
+				lotId.add(id);
+		    }          
+
+		} catch (SQLException ex) {
+		    ex.printStackTrace();
+		    throw ex;
+		}      
+
+		return lotId;
 	}
 }
