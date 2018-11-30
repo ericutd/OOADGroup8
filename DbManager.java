86ada7d9a78f601430b530a9e9e6fe9111094237
ericutd <43154071+ericutd@users.noreply.github.com>
2018-11-30 14:18:26 -0600

    Changed to fix errors and allow for inserting permits into the database

---
+++ DbManager.java
@@ -0,0 +1,53 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+/**
+ * 
+ * @author mehra
+ * getConnection method help us to connect to the appropriate database. In this project we only have one
+ * database. Data comes from MyDB interface.
+ */
+public class DbManager implements MyDB{
+	
+	private static DbManager dbManager;
+	
+	private DbManager() {
+		
+	}
+	
+	public static DbManager getInstance() {
+		if(dbManager == null) {
+			dbManager = new DbManager();
+		}
+		return dbManager;
+	}
+
+	public Connection getConnection(){
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection myConnection = DriverManager.getConnection(CONN_URL,USER,PASS);
+			return myConnection;
+		}catch(Exception e){
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public ResultSet execute(String sql) throws SQLException {
+		Connection connection = getConnection();
+		PreparedStatement ps = connection.prepareStatement(sql);
+		ResultSet rs = ps.executeQuery();
+		return rs;
+	}
+	
+	public void insert(String sql) throws SQLException {
+		Connection connection = getConnection();
+		Statement s = connection.createStatement();
+		s.executeUpdate(sql);
+	}
+}
